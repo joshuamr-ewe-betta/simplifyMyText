1955650ca293f8b1280e3379fb59879c7669a52f
Joshua Meriyah <joshua.meriyah@pathtoagility.com>
2024-09-27 14:21:30 -0400

    Trying logging for vars

--- src/app/handlers/submitTextToSimplify.ts
+++ src/app/handlers/submitTextToSimplify.ts
@@ -1,7 +1,5 @@
 'use server';
 
-import { promises as fs } from 'fs';
-
 import Anthropic from '@anthropic-ai/sdk';
 import { anthropicSystemContent } from './anthropicSystem';
 
@@ -12,6 +10,7 @@ const anthropic = new Anthropic({
 export async function submitTextToSimplify(
   htmlEntered: string
 ): Promise<{ simplifiedVersion: string }> {
+  console.log('process.env.ANTHROPIC_API_KEY', process.env.ANTHROPIC_API_KEY);
   const msg = await anthropic.messages.create({
     model: 'claude-3-5-sonnet-20240620',
     max_tokens: 1000,
